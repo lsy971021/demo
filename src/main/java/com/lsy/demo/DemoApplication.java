5c25af9a40c0114bbb63477500d51a8f6190b412
lsy <lsy971021@gmail.com>
2025-03-31 21:49:41 +1030

    output ip address

--- src/main/java/com/lsy/demo/DemoApplication.java
+++ src/main/java/com/lsy/demo/DemoApplication.java
@@ -3,11 +3,33 @@ package com.lsy.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("1111");
+		try {
+			Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+			while (interfaces.hasMoreElements()) {
+				NetworkInterface networkInterface = interfaces.nextElement();
+				Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+				while (addresses.hasMoreElements()) {
+					InetAddress address = addresses.nextElement();
+					if (!address.isLoopbackAddress() && address instanceof Inet4Address) {
+						System.out.println("服务器 IP 地址: " + address.getHostAddress());
+					}
+				}
+			}
+		} catch (SocketException e) {
+			System.err.println("无法获取服务器 IP 地址");
+			e.printStackTrace();
+		}
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
